9ba03b5fdaa658e687f4ba7e4e9ca0fcd446a536
muhammadrisano <muhammadrisano@gmail.com>
2024-02-11 10:21:26 +0700

    fix: fixing worker profile

--- src/routes/workers.js
+++ src/routes/workers.js
@@ -5,8 +5,9 @@ const  {protect} = require('../middlewares/auth')
 
 router.post('/register', workerController.register)
 router.get('/', protect, workerController.selectAll)
-router.get('/:id', workerController.detail)
+
 router.get('/profile',protect, workerController.profile)
 router.put('/profile',protect, workerController.update)
+router.get('/:id', workerController.detail)
 
 module.exports = router
