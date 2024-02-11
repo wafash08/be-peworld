bc665eb9ffbb2862170ec01be96a742fd14e4898
muhammadrisano <muhammadrisano@gmail.com>
2024-02-11 11:04:30 +0700

    fix: endpoint hire

--- src/models/hire.js
+++ src/models/hire.js
@@ -6,7 +6,7 @@ const create = ({id, message_purpose, worker_id, recruiter_id, name, email, phon
   );
 }
 const selectAll = ({filterBy, filterValue}) =>{
-  return pool.query(`SELECT * FROM hire ${filterBy ? `WHERE ${filterBy}= $1`: ''} `, [filterValue])
+  return pool.query(`SELECT hire.id, hire.message_purpose, hire.worker_id, hire.recruiter_id, hire.name AS name_request_hire, hire.email AS email_request_hire, hire.phone AS phone_request_hire, hire.desciption AS desciption_request_hire, hire.created_at, hire.updated_at, workers.name AS worker_name, workers.phone AS worker_phone, workers.job_desk AS worker_job_desk, workers.domicile AS worker_domicile, workers.workplace AS worker_workplace, workers.photo AS worker_photo, recruiters.name AS recruiter_name, recruiters.company AS recruiter_company , recruiters.position AS recruiter_position, recruiters.photo AS recruiter_photo FROM hire join workers ON hire.worker_id = workers.id join recruiters ON hire.recruiter_id = recruiters.id  ${filterBy ? `WHERE ${filterBy}= $1`: ''} `, [filterValue])
 }
 
 module.exports = {
